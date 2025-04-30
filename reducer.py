14b10824c9be84d8b668d69de116c4103e13a5db
chelseaa1808 <cta1@usf.edu>
2025-04-29 21:53:53 -0400

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -33,6 +33,6 @@ for line in sys.stdin:
         current_count = count
         current_word = word
 
-# do not forget to output the last word if needed!
+# output the last word if needed!
 if current_word == word:
     print '%s\t%s' % (current_word, current_count)
